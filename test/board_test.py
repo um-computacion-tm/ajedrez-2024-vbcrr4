69261da5609468d6f1112eb1dee7eb7dd64510b1
vbcrr4 <v.becerra@alumno.um.edu.ar>
2024-08-15 20:23:29 -0300

    agregando test de board

---
+++ test/board_test.py
@@ -0,0 +1,45 @@
+import unittest
+
+from game.board import Board
+from game.piece import *
+
+class TestBoard(unittest.TestCase):
+
+    def test_init(self):
+        board = Board()
+
+        # Check if the board is initialized with a 8x8 matrix
+        self.assertEqual(len(board.__positions__), 8)
+        for row in board.__positions__:
+            self.assertEqual(len(row), 8)
+
+        # Check if the white pieces are placed correctly
+        self.assertIsInstance(board.__positions__[0][0], Rook)
+        self.assertIsInstance(board.__positions__[0][1], Knight)
+        self.assertIsInstance(board.__positions__[0][2], Bishop)
+        self.assertIsInstance(board.__positions__[0][3], Queen)
+        self.assertIsInstance(board.__positions__[0][4], King)
+        self.assertIsInstance(board.__positions__[0][5], Bishop)
+        self.assertIsInstance(board.__positions__[0][6], Knight)
+        self.assertIsInstance(board.__positions__[0][7], Rook)
+
+        # Check if the black pieces are placed correctly
+        self.assertIsInstance(board.__positions__[7][0], Rook)
+        self.assertIsInstance(board.__positions__[7][1], Knight)
+        self.assertIsInstance(board.__positions__[7][2], Bishop)
+        self.assertIsInstance(board.__positions__[7][3], Queen)
+        self.assertIsInstance(board.__positions__[7][4], King)
+        self.assertIsInstance(board.__positions__[7][5], Bishop)
+        self.assertIsInstance(board.__positions__[7][6], Knight)
+        self.assertIsInstance(board.__positions__[7][7], Rook)
+
+        # Check if the white pawns are placed correctly
+        for i in range(8):
+            self.assertIsInstance(board.__positions__[1][i], Pawn)
+
+        # Check if the black pawns are placed correctly
+        for i in range(8):
+            self.assertIsInstance(board.__positions__[6][i], Pawn)
+
+if __name__ == '__main__':
+    unittest.main()
