ec58d8db842714941f8fcde55eed41e96fbb60e6
vbcrr4 <v.becerra@alumno.um.edu.ar>
2024-11-05 14:27:07 -0300

    refactorizando cli

--- test/test_cli.py
+++ test/test_cli.py
@@ -53,23 +53,6 @@ class TestChessCLI(unittest.TestCase):
             mock_instance.play.assert_not_called()  # No debería intentar jugar con entrada inválida
             self.assertIn("Turno de las", salida)
 
-    @patch('builtins.input', side_effect=['A11', 'A3', 'Q'])  # Secuencia de inputs inválidos
-    @patch('game.cli.Game')
-    def test_iniciar_juego_invalid_input2(self, MockGame, mock_input):
-        """Simula entradas inválidas del usuario para verificar el manejo de errores."""
-        mock_game_instance = MockGame.return_value
-
-        # Asegurarse de que game_over devuelva False hasta que se envíe la entrada 'Q'
-        mock_game_instance.game_over.side_effect = [False, False, True]
-
-        with patch('sys.stdout', new=StringIO()) as fake_out:
-            iniciar_juego()
-            output = fake_out.getvalue()
-
-            # Validar mensajes de error y manejo de entradas inválidas
-            self.assertIn("Entrada inválida en la posición inicial. Asegúrate de ingresar las posiciones en formato correcto (ej. A2, H8).", output)
-            mock_game_instance.play.assert_not_called()  # No debería intentar jugar con entrada inválida
-            self.assertIn("Turno de las", output)
     @patch('builtins.input', side_effect=['Q'])
     @patch('game.cli.Game')
     def test_iniciar_juego_exit(self, MockGame, mock_input):
