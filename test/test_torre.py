099f5d599ee0d69a985e29481dbc3ae2073d9107
vbcrr4 <v.becerra@alumno.um.edu.ar>
2024-11-05 14:35:56 -0300

    refactorizando torre

--- test/test_torre.py
+++ test/test_torre.py
@@ -6,16 +6,17 @@ class TestTorre(unittest.TestCase):
         #inicializacion de tablero
         self.__positions__ = [[None] * 8 for _ in range(8)]
         self.__torre__ = Torre('Black',(3, 3))
+        self.__posicionocupada__ = Torre("White", (4, 3))
         self.__positions__[1][1] = self.__torre__
+        self.__positions__[4][3] = self.__posicionocupada__
 
     def test_assign_value(self):
         self.assertEqual(self.__torre__.assign_value(), 5)
 
     def test_valid_white_move(self):
-        self.assertTrue(self.__torre__.piece_move(self.__positions__,(5, 3)))
+        self.assertTrue(self.__torre__.piece_move(self.__positions__,(3, 5)))
     
     def test_torre_blocked_move(self):
-        self.__positions__[4][3] = Torre("White", (4, 3))
         self.assertFalse(self.__torre__.piece_move(self.__positions__, (6, 3)))
 
 if __name__ == '__main__':
